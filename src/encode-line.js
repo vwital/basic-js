4f153d9c2636c50d6d3dc3af27b672fed9856e58
vwital <wital128@tut.by>
2023-12-13 21:04:17 +0300

    feat: encode-line solved

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let symCounter = 1;
+  let finalStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      symCounter += 1;
+    } else {
+      if (symCounter === 1) {
+        finalStr += `${str[i]}`;
+      } else {
+        finalStr += `${symCounter}${str[i]}`;
+        symCounter = 1;
+      }
+    }
+  }
+  return finalStr;
 }
 
+console.log(encodeLine("aabbbc"));
+console.log(encodeLine("abbcca"));
+
 module.exports = {
-  encodeLine
+  encodeLine,
 };
+
+//Если нужно подсчитать количество одинаковых символов в разнобой
+
+// let currentQuantity = 1;
+//   let wereCharacters = [];
+//   let resultStr = "";
+//   for (i = 0; i < str.length; i++) {
+//     if (!wereCharacters.includes(str[i])) {
+//       for (j = i + 1; j < str.length; j++) {
+//         if (str[i] === str[j]) {
+//           currentQuantity++;
+//         }
+//       }
+//       if (currentQuantity === 1) resultStr += str[i];
+//       if (currentQuantity > 1) {
+//         resultStr += `${currentQuantity.toString()}${str[i]}`;
+//       }
+//       currentQuantity = 1;
+//       wereCharacters.push(str[i]);
+//     }
+//   }
+//   return resultStr;
