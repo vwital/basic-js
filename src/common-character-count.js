380e153048f615f20013e9bdb011801dca2802e2
vwital <wital128@tut.by>
2023-12-13 16:24:28 +0300

    feat: common-character count-cats solved

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  let charCounter = 0;
+  for (let i = 0; i < s1.length; i++) {
+    console.log("sym", s1[i]);
+    for (let j = 0; j < s2.length; j++) {
+      if (s1[i] === s2[j] && s1[i] !== " ") {
+        charCounter += 1;
+        s2 = s2.replace(new RegExp(s2[j]), " ");
+        s1 = s1.replace(new RegExp(s1[i]), " ");
+      }
+    }
+  }
+  return charCounter;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let catsCounter = 0;
+  for (i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === "^^") catsCounter++;
+    }
+  }
+  return catsCounter;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
